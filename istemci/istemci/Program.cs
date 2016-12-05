cb9e3bb1ab950be93c2ca912cb832773590b047d
gursuasik <gursuasik@gmail.com>
2016-12-05 14:51:01 +0300

    Network Socket

---
+++ istemci/istemci/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace istemci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try 
+            
+            {
+                while (true)
+                {
+                    TcpClient tc = new TcpClient();
+                    //tc.Connect("10.37.35.65", 8000);
+                    tc.Connect(IPAddress.Loopback, 8000);
+                    
+                    String str = Console.ReadLine();
+                    NetworkStream stm = tc.GetStream();
+                    ASCIIEncoding asen = new ASCIIEncoding();
+                    byte[] gon = asen.GetBytes(str);
+                    //Console.WriteLine("Transmitting");
+
+                    stm.Write(gon, 0, gon.Length); // veri gönder
+                    byte[] okunan = new byte[100];
+                    int ok = stm.Read(okunan, 0, 100);
+
+                    for (int i = 0; i < ok; i++)
+                        Console.Write(Convert.ToChar(okunan[i]));
+
+                    tc.Close();
+                }
+            
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("hata oldu" + e.StackTrace);
+            
+            }
+
+            Console.Read();
+
+
+
+
+
+        }
+    }
+}
---
+++ sunucu/sunucuistemci/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+
+using System.Text;
+
+namespace sunucuistemci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+               //IPAddress ip= IPAddress.Parse("localhost");
+                while (true)
+                {
+                    TcpListener tcpl = new TcpListener(8000);
+                    tcpl.Start();
+                    Socket s = tcpl.AcceptSocket();
+
+                    byte[] veri = new byte[100];
+                    int okunan = s.Receive(veri);
+                    for (int i = 0; i < okunan; i++)
+                        Console.Write(Convert.ToChar(veri[i]));
+
+                    Console.WriteLine();
+                    ASCIIEncoding a = new ASCIIEncoding();
+                    s.Send(a.GetBytes("Appeared...\n"));
+                    s.Close();
+                    tcpl.Stop();
+                }
+                
+
+
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("hata oldu :"+e.StackTrace);
+            
+            
+            }
+
+           
+
+
+
+
+        }
+    }
+}
